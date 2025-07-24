3f3e48426487df6bc8b0431e0337b3ca6938b1d0
Anamika Ghosh Dola <anamikaghosh443@gmail.com>
2025-07-24 20:28:09 +0600

    Add files via upload

---
+++ Alphanumeric character detection.py
@@ -0,0 +1,14 @@
+x = input("Enter a number : ")
+
+if x >= "a" and x <= "z":
+    print("Alphabet")
+elif x >= "A" and x <= "Z":
+    print("Alphabet")
+elif x >= '0' and x <= '9':
+    print("Digit")
+else:
+    print("Special Character")
+
+
+
+
